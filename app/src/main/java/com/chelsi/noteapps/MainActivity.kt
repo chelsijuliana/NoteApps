6db3a4c59da6b31dc6c45d33a273b1ea615fb510
Chelsi Juliana Rizky <chelsijuliana10@gmail.com>
2024-11-19 17:07:56 +0700

    Initial commit

---
+++ app/src/main/java/com/chelsi/noteapps/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.chelsi.noteapps
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.chelsi.noteapps.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: NoteDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(), this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val notes = db.getAllNotes()
+        Log.d("MainActivity", "Notes count: ${notes.size}")
+        notesAdapter.refreshData(notes)
+    }
+}
---
+++ app/src/main/java/com/chelsi/noteapps/Note.kt
@@ -0,0 +1,7 @@
+package com.chelsi.noteapps
+
+data class Note(
+    val id: Int,
+    val title: String,
+    val content: String
+)
